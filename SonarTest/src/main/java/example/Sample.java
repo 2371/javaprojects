d3879c5d9d4e4fea4d5130046d6708585738d8be
2371 <skmohdrafi@gmail.com>
2017-04-20 16:06:50 +0530

    Removed unnecessary comments

--- SonarTest/src/main/java/example/Sample.java
+++ SonarTest/src/main/java/example/Sample.java
@@ -4,11 +4,9 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//double c;
-		//c = 4/0;
-		//c = 4/1;
+		
 		System.out.println("Welcome to the maven sonar test project");
-		//System.out.println("the value of c is:"+c);
+		
 
 	}
 
